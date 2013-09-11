6b591e58e2dfe73670f0c3c3805e89821eece5b9
alexandermitchell <alex.mitchell@mac.com>
2013-09-11 18:32:04 -0400

    Refactoring the products controller to respond to JSON

--- rainforest/app/controllers/products_controller.rb
+++ rainforest/app/controllers/products_controller.rb
@@ -1,42 +1,88 @@
 class ProductsController < ApplicationController
+  
+  # GET /products
+  # GET /products.json
   def index
   	@products = Product.all
+
+  	respond_to do |format| 
+  		format.html #index.html.erb
+  		format.json { render json: @products } 
+  	end
+
   end
 
+  # GET /products/1
+  # GET /products/1.json
   def show
   	@product = Product.find(params[:id])
+
+  	respond_to do |format| 
+  		format.html #show.html.erb
+  		format.json { render json: @product }
+  	end
   end
 
+  # GET /products/new
+  # GET /products/new.json
   def new
   	@product = Product.new
+
+  	respond_to do |format| 
+  	format.html #new.html.erb
+  	format.json { render json: @product }
+  	end
+
   end
 
+  # GET /products/edit/1
+  # GET /products/edit/1.json
   def edit
   	@product = Product.find(params[:id])
+
+  	respond_to do |format|
+  		format.html #edit.html.erb
+  		format.json { render json: @product }
+  	end
+
   end
 
   def create
-  	@product = Product.new(params[:product])
-  	if product.save
-  	redirect_to products_url
-  	else
-  	render :new
-  	end
+  	@product = Product.new(product_params)
+
+  	respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render json: @product, status: :created, location: @product }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
   end
 
   def update
   	@product = Product.find(params[:id])
-  	if product.update_attributes(params[:product])
-  		redirect_to product_path(@product)
-  	else
-  		render :edit
-  	end
+  	
+	respond_to do |format|
+	      if @product.update_attributes(product_params)
+	        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+	        format.json { head :no_content }
+	      else
+	        format.html { render action: "edit" }
+	        format.json { render json: @product.errors, status: :unprocessable_entity }
+	      end
+	end
+
   end
 
   def destroy
   	@product = Product.find(params[:id])
   	@product.destroy
-  	redirect_to products_path
+
+  	respond_to do |format|
+      format.html { redirect_to products_url }
+      format.json { head :no_content }
+    end
   end
 end
 
