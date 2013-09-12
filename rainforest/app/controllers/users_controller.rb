1bf9c5018fb56237c13b841c7473409586d72487
alexandermitchell <alex.mitchell@mac.com>
2013-09-12 14:04:15 -0400

    Creating Login Functionality

---
+++ rainforest/app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.find(user_params)
+  	if @user.save
+  		redirect_to products_url, :notice => "Signed up!"
+  	else
+  		render :new
+  	end
+  end
+
+end
+
+private
+
+	def user_params
+			params.require(:user)
+				permit(:email, :password, :password_confirmation)
+	end
--- rainforest/config/routes.rb
+++ rainforest/config/routes.rb
@@ -1,4 +1,8 @@
 Rainforest::Application.routes.draw do
+  get "users/new"
+  get "users/create"
   root "products#index"
   resources :products
+  resources :users, :only => [:new, :create]
+  resources :sessions, :only => [:new, :create, :destroy]
 end
